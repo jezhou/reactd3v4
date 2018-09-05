70e935d140ebf2cbe640f92f79ece713d133f931
jezhou <jesse.zhou@gusto.com>
2018-09-05 02:29:53 -0700

    page 47 (data won't parse correctly)

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import Preloader from './components/Preloader';
 import './App.css';
 
+import * as d3 from 'd3';
+import _ from 'lodash';
+import { loadAllData } from './Datahandling';
+
 class App extends Component {
   state = {
-    techSalaries: []
+    techSalaries: [],
+    countyNames: [],
+    medianIncomes: []
+  }
+
+  componentWillMount() {
+    loadAllData(data => this.setState(data));
   }
   render() {
     if (this.state.techSalaries.length < 1) {
       return <Preloader/>;
     }
     return (
-      <div className="App">
+      <div className="App container">
+        <h1>Loaded {this.state.techSalaries.length}</h1>
       </div>
     );
   }
