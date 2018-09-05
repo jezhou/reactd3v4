562f37220b2d9ddfed247052612c58ffe0ad0384
jezhou <jesse.zhou@gusto.com>
2018-09-05 01:55:59 -0700

    page 42

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Preloader from './components/Preloader';
 import './App.css';
 
 class App extends Component {
+  state = {
+    techSalaries: []
+  }
   render() {
+    if (this.state.techSalaries.length < 1) {
+      return <Preloader/>;
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
